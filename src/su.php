84eb75a0e5cf9205ceabe223480b4270dc9b7daf
Zhdanov Valery <becwal@yandex.ru>
2021-04-02 22:54:05 +0300

    Исправление подключения CurrencyRateCbr

--- src/su.php
+++ src/su.php
@@ -1,20 +1,20 @@
 <?php
+ include 'vendor/autoload.php';
+ use AndyDune\CurrencyRateCbr\DailyRate; 
 
 class Super{
    protected $item;
    
-   use AndyDune\CurrencyRateCbr\DailyRate;
 
    function __construct()
    {
-
-            include __DIR__.'./vendor/autoload.php';
+   
+    //use AndyDune\CurrencyRateCbr\DailyRate;        
             $rate = new DailyRate();
             $rate->setDate(new \DateTime()); // не оюязательно - по умолчанию используется текущая дата 
             $isOk = $rate->retrieve(); // true если данные успешно получены
 
             // Извлекаем курс доллара 
-
             /** @var DailyRateItem $item */
             $this->item = $rate->get('usd'); // код валюты, регистр не важен
    }
@@ -25,7 +25,7 @@ class Super{
     function getdollar()
     {
  
-        echo $item->getCharCode(); // код валюты: USD
-        echo $item->getValue(); // цена: 63,1394
+        echo $this->item->getCharCode(); // код валюты: USD
+        echo $this->item->getValue(); // цена: 63,1394
     }
 }
