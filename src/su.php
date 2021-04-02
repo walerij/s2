2eb856a6ef5751f34988912c1fff1a32cce612b9
Zhdanov Valery <becwal@yandex.ru>
2021-04-02 22:22:57 +0300

    include lib'

--- src/su.php
+++ src/su.php
@@ -1,6 +1,31 @@
 <?php
 
 class Super{
+   protected $item;
+   
+   use AndyDune\CurrencyRateCbr\DailyRate;
+
+   function __construct()
+   {
+
+            include __DIR__.'./vendor/autoload.php';
+            $rate = new DailyRate();
+            $rate->setDate(new \DateTime()); // не оюязательно - по умолчанию используется текущая дата 
+            $isOk = $rate->retrieve(); // true если данные успешно получены
+
+            // Извлекаем курс доллара 
+
+            /** @var DailyRateItem $item */
+            $this->item = $rate->get('usd'); // код валюты, регистр не важен
+   }
+
     function you($name="")
     { return "Ты супер,".$name; }
+
+    function getdollar()
+    {
+ 
+        echo $item->getCharCode(); // код валюты: USD
+        echo $item->getValue(); // цена: 63,1394
+    }
 }
